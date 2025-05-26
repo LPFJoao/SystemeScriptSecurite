e18eda49e464b91d7faf55b2066c3155b5671ef5
João Silva <joao.silva-coutinho@laplateforme.io>
2025-05-26 11:02:11 +0200

    Update setup_user.sh

--- Securiser_scripts/scripts/setup_user.sh
+++ Securiser_scripts/scripts/setup_user.sh
@@ -6,7 +6,6 @@ sudo apt install mariadb-server -y
 sudo mysql -e "DELETE FROM mysql.user WHERE User='';"
 sudo mysql -e "DROP DATABASE IF EXISTS test;"
 sudo mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-sudo mysql -e "FLUSH PRIVILEGES;"
 sudo mysql -e "CREATE USER IF NOT EXISTS 'dev'@'localhost' IDENTIFIED BY 'devpass';"
 sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'dev'@'localhost' WITH GRANT OPTION;"
 sudo mysql -e "FLUSH PRIVILEGES;"
